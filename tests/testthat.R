2145399aca59839663224bb09ec66218b8874793
Jon Harmon <jonthegeek@gmail.com>
2020-04-29 10:42:06 -0500

    Switched to httptest

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,5 +1,4 @@
 library(testthat)
-# library(vcr)
 library(aws.lambda)
 
 test_check("aws.lambda")
--- tests/testthat/test-http.R
+++ tests/testthat/test-http.R
@@ -1,40 +1,22 @@
 test_that("lambdaHTTP works", {
-  skip(
-    paste(
-      "This test does not work yet except when run manually,",
-      "and I don't know why."
-    )
-  )
-
   # For these tests, we're still using mocks, but something closer to the real
   # response.
-  library(vcr)
-  vcr::vcr_configure(
-    dir = here::here("tests", "testthat"),
-    allow_unused_http_interactions = FALSE,
-    record = "none"
-  )
 
-  expect_warning(
-    vcr::use_cassette(
-      "account_httr",
-      {
-        mockery::stub(
-          where = lambdaHTTP,
-          what = "aws.signature::signature_v4_auth",
-          how = list(
-            BodyHash = "fake_body_hash",
-            SignatureHeader = "fake_signature_header"
-          )
+  httptest::with_mock_api(
+    {
+      mockery::stub(
+        where = lambdaHTTP,
+        what = "aws.signature::signature_v4_auth",
+        how = list(
+          BodyHash = "fake_body_hash",
+          SignatureHeader = "fake_signature_header"
         )
-        test_result <- names(lambdaHTTP(
-          verb = "GET",
-          action = "/2016-08-19/account-settings"
-        ))
-      },
-      record = "none"
-    ),
-    "partial match"
+      )
+      test_result <- names(lambdaHTTP(
+        verb = "GET",
+        action = "/2016-08-19/account-settings"
+      ))
+    }
   )
 
   expected_result <- c(
